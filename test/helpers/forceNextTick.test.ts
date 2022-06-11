871bf9963989cea086fa0350d700e9016343e4d9
brofrain <downloading130@gmail.com>
2022-06-11 11:01:55 +0200

    refactor helpers

---
+++ test/helpers/forceNextTick.test.ts
@@ -0,0 +1,15 @@
+describe('forceNextTick', () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('should request animation frame twice', async () => {
+    const requestAnimationFrameMock = vi
+      .spyOn(window, 'requestAnimationFrame')
+      .mockImplementation((cb: any) => cb());
+
+    await forceNextTick();
+
+    expect(requestAnimationFrameMock).toHaveBeenCalledTimes(2);
+  });
+});
